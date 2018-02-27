b78051c52d482e7c73d66c2579281309a3f4fec2
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-02-27 16:45:17 -0600

    java practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,103 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World! Second Line!");
+//        Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+
+//        Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+//        String myString = "This is my first string created in Java";
+//        System.out.println(myString);
+
+//        Change your code to assign a character value to myString. What do you notice?
+//        char myString = 'c';
+//        System.out.println(myString);
+
+//        Change your code to assign the value 3.14159 to myString. What happens?
+//        double myString = 3.14159;
+//        System.out.println(myString);
+
+//        Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+//        long myNumber;
+//        System.out.println(myNumber);
+
+//        Change your code to assign the value 3.14 to myNumber. What do you notice?
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+//        Change your code to assign the value 123 to myNumber.
+//        int myNumber = 123;
+//        System.out.println(myNumber);
+
+//        Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+//          float myNumber = 3.14;
+//          System.out.println(myNumber);
+
+//                Copy and paste the following code blocks one at a time and execute them
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x++);
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(++x);
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        What is the difference between the above code blocks? Explain why the code outputs what it does.
+//        The x++ print x before increment
+//        The ++x increment then print incremented number
+
+
+//        Try to create a variable named class. What happens?
+//        int class = 5    (class is a reserved word in java)
+
+//        Rewrite the following expressions using the relevant shorthand assignment operators:
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+
+
+        // How java code runs
+        // write java in a .java file
+        // compile your java code with java compiler
+        // java compiler gives you a .class file, which is byte code
+        // JVM java virtual machine takes byte code and interprets it in machine code
+
     }
 }
 
