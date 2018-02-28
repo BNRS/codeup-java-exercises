b839f27006d0b8bd7401abe48988bc509eebcc90
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-02-28 15:50:29 -0600

    control flow ex

---
+++ src/Bob.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+//        Create a class Bob for the following exercise with a main method. Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+//        Bob answers 'Sure.' if you ask him a question.
+//        He answers 'Whoa, chill out!' if you yell at him.
+//        He says 'Fine. Be that way!' if you address him without actually saying anything.
+//        He answers 'Whatever.' to anything else.
+
+//        Scanner sc = new Scanner(System.in);
+//        boolean question = true;
+//        boolean yell = true;
+//        boolean whatever = true;
+//
+//        System.out.print("Ask bob a question...anything..really..: " );
+//        String sureResponse = sc.nextLine();
+//        if (question == true) {
+//            sureResponse = "Sure";
+//            System.out.println(sureResponse);
+//        }
+//        System.out.print("Bob does not like it when you yell at him..so..yell at him!");
+//        String yellResponse = sc.nextLine();
+//        if (yell == true)
+//            yellResponse ="Whoa chill out man!";
+//            System.out.println(yellResponse);
+//
+//        System.out.print("Say anything you want to, to Bob..: ");
+//        String whateverResponse = sc.nextLine();
+//        if (whatever == true)
+//            whateverResponse ="Whatever dude";
+//        System.out.println(whateverResponse);
+
+
+
+
+
+    }
+}
